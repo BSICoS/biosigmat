6713fbbeb48d6052029e030674eb703e7ed602bc
Diego Cajal <dcajal95@gmail.com>
2025-05-23 13:40:58 +0200

    Refactor nanfiltfilt function and update test cases

--- src/tools/nanfiltfilt.m
+++ src/tools/nanfiltfilt.m
@@ -1,4 +1,4 @@
-function y = nanfiltfilt(bb, aa, x, maxgap)
+function y = nanfiltfilt(b, a, x, maxgap)
 % NANFILTFILT Implements filtfilt function with support for NaN values
 %
 % This function applies zero-phase digital filtering to a signal that contains
@@ -7,8 +7,8 @@ function y = nanfiltfilt(bb, aa, x, maxgap)
 % the specified maximum gap.
 %
 % Inputs:
-%   bb         - Numerator coefficients of the filter
-%   aa         - Denominator coefficients of the filter
+%   b          - Numerator coefficients of the filter
+%   a          - Denominator coefficients of the filter
 %   x          - Input signal with possible NaN values
 %   maxgap     - Optional. Maximum gap size to interpolate. If not specified,
 %                all NaN segments will be preserved regardless of their size.
@@ -42,15 +42,15 @@ end
 signalFilled = fillmissing(x, 'linear');
 
 % Apply filter
-signalFiltered = filtfilt(bb, aa, signalFilled);
+signalFiltered = filtfilt(b, a, signalFilled);
 
 % Initialize output with filtered signal
 y = signalFiltered;
 
-% Find sequences of NaN values
+% Find sequences of NaN values. Sequences larger than maxgap will be filled with NaN again
 seqs = findsequences(double(idxNan));
-seqs(seqs(:,1) == 0, :) = [];
-seqs(seqs(:,4) <= maxgap, :) = [];
+seqs(seqs(:,1) == 0, :) = []; % Remove zero-value sequences. NaN sequences are one-value sequences
+seqs(seqs(:,4) <= maxgap, :) = []; % Remove sequences shorter than maxgap. Interpolation will be kept for these sequences
 
 % Restore NaN values for sequences longer than maxgap
 for idx = 1:size(seqs, 1)

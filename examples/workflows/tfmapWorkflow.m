4d5372550c75db7928fbb41323c7e9ef7d6280b3
Diego Cajal <dcajal95@gmail.com>
2025-07-15 17:38:17 +0200

    Add tfmapWorkflow.m to demonstrate time-frequency mapping of synthetic signals

---
+++ examples/workflows/tfmapWorkflow.m
@@ -0,0 +1,51 @@
+% TFMAP WORKFLOW
+%
+% This workflow demonstrates how to create a time-frequency map of a synthetic signal
+% using the nanpwelch function and slicesignal for segmentation.
+%
+% The workflow:
+% 1. Generates a synthetic chirp signal with variable dominant frequency
+% 2. Slices the signal into 20-second segments with 50% overlap
+% 3. Computes power spectral density matrix using nanpwelch
+% 4. Creates a time-frequency plot
+
+% Add required paths for source code
+addpath(fullfile('..', '..', 'src', 'tools'));
+
+fs = 256;
+signalDuration = 120;
+t = (0:1/fs:signalDuration)';
+
+% Generate synthetic chirp signal with variable dominant frequency
+% Chirp from 5 Hz to 30 Hz over the signal duration
+f0 = 5;
+f1 = 30;
+signal = chirp(t, f0, signalDuration, f1) + 0.1 * randn(size(t));
+signal = signal(:);
+
+% Slice signal into 20-second segments with 50% overlap
+segmentDuration = 20;
+segmentSamples = round(segmentDuration * fs);
+overlapPercent = 0.5;
+overlapSamples = round(segmentSamples * overlapPercent);
+
+[slicedSignal, tcenter] = slicesignal(signal, segmentSamples, overlapSamples, fs);
+
+% Compute power spectral density matrix using pwelch
+windowLength = 512;
+noverlap = round(windowLength * 0.5);
+nfft = 1024;
+
+[pxx, f] = nanpwelch(slicedSignal, windowLength, noverlap, nfft, fs, []);
+
+% Create time-frequency plot
+figure;
+imagesc(tcenter, f, 10*log10(pxx));
+axis xy;
+xlabel('Time (s)');
+ylabel('Frequency (Hz)');
+title('Time-Frequency Map of Chirp Signal');
+colorbar;
+ylabel(colorbar, 'Power (dB)');
+ylim([0 50]);
+grid on;
